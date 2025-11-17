a8df7b935d60cd17ac8bfb9049d09e9b0cfb25ae
Muti <mutiarapandejlaki47@gmail.com>
2025-11-18 00:25:31 +0800

    buat desain form cek stok gudang

--- sources/view/pages/inventory-product/history-inventory-data.jsx
+++ sources/view/pages/inventory-product/history-inventory-data.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import NavProduct from "../../../components/base/nav-product";
 import SearchFilter from "../../../components/base/search-filter";
 import InventoryProduct from "../../../components/product-page/inventory-items";
+import CheckStock from "../../../components/product-page/check-stock";
 
 function HistoryInventoryData() {
   return(
@@ -14,6 +15,9 @@ function HistoryInventoryData() {
           </div>
           <SearchFilter />
           <InventoryProduct />
+        </div>
+        <div className="checking-overlay">
+          <CheckStock />
         </div>
       </div>
     </div>
