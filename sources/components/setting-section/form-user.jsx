bedf835848b2c9636d91f36e3174ab04e8d75d9d
Muti <mutiarapandejlaki47@gmail.com>
2025-11-19 14:35:05 +0800

    tambah icon di form tambah pengguna

--- sources/components/setting-section/form-user.jsx
+++ sources/components/setting-section/form-user.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import IconAddUser from "../../assets/icon/material-symbols_person-add-outline.svg?react";
 import IconCancel from "../../assets/icon/material-symbols_cancel.svg?react";
+import IconUser from "../../assets/icon/material-symbols_person-outline.svg?react";
+import IconEmail from "../../assets/icon/ic_outline-email.svg?react";
+import IconPassword from "../../assets/icon/mdi_password-outline.svg?react";
+import IconPosition from "../../assets/icon/icon-park-outline_user-positioning.svg?react";
+import IconStatus from "../../assets/icon/eos-icons_role-binding-outlined.svg?react";
 
 function FormUser() {
   return(
@@ -10,27 +15,27 @@ function FormUser() {
           <IconAddUser className="icon darkGreenIcon" />
           <p>Tambah Pengguna</p>
         </div>
-        <IconCancel className="icon" />
+        <IconCancel className="icon blackIcon" />
       </div>
       <form action="" className="main-form">
         <div className="inputan">
-          <label htmlFor="">Nama Pengguna</label>
+          <label htmlFor=""><IconUser className="icon darkGreenIcon" />Nama Pengguna</label>
           <input type="text" placeholder="Masukkan nama pengguna" />
         </div>
         <div className="inputan">
-          <label htmlFor="">Email</label>
+          <label htmlFor=""><IconEmail className="icon darkGreenIcon" />Email</label>
           <input type="text" placeholder="Masukkan email pengguna" />
         </div>
         <div className="inputan">
-          <label htmlFor="">Password</label>
+          <label htmlFor=""><IconPassword className="icon darkGreenIcon" />Password</label>
           <input type="text" placeholder="Masukkan password pengguna" />
         </div>
         <div className="inputan">
-          <label htmlFor="">Jabatan</label>
+          <label htmlFor=""><IconPosition className="icon darkGreenIcon" />Jabatan</label>
           <input type="text" placeholder="Jabatan pengguna di kantor" />
         </div>
         <div className="inputan">
-          <label htmlFor="">Status</label>
+          <label htmlFor=""><IconStatus className="icon darkGreenIcon" />Status</label>
           <select name="Ukuran Satuan" id="">
             <option value="admin">Admin</option>
             <option value="owner">Owner</option>
