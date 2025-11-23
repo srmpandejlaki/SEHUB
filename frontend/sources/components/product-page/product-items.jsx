fabdf2a20d75ace07a52f4b1d284c2607a8e09a7
Muti <mutiarapandejlaki47@gmail.com>
2025-11-23 17:18:20 +0800

    hapus component" button

--- frontend/sources/components/product-page/product-items.jsx
+++ frontend/sources/components/product-page/product-items.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import EditBtn from "../button/edit-btn";
-import DeleteBtn from "../button/delete-btn";
 import Sirop1Img from "../../assets/public/gambar-produk/seho-sirop1.png";
 import DefaultImg from "../../assets/public/gambar-produk/default-img.png";
 import Granule1Img from "../../assets/public/gambar-produk/seho-granule1.png";
