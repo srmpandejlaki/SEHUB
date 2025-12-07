6ea74e9bc43b9435ca6d7b849817fae067f42a90
Muti <mutiarapandejlaki47@gmail.com>
2025-12-07 19:43:14 +0800

    pindah folder

--- backend/src/routes/ProductRoute.js
+++ backend/src/routes/ProductRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { upload } from "../utils/middlewares/upload.js";
+import { upload } from "../middlewares/upload.js";
 import ProductController from "../controllers/ProductController.js";
 
 const routerProducts = express.Router();
