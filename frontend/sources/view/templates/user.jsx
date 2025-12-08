89831e412711250e0dfa396a45d705c2d1791a63
Muti <mutiarapandejlaki47@gmail.com>
2025-12-09 07:26:18 +0800

    tambah fungsi show password

--- frontend/sources/view/templates/user.jsx
+++ frontend/sources/view/templates/user.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import IconAdmin from "../../assets/icon/clarity_employee-line.svg?react";
 import IconOwner from "../../assets/icon/fluent-mdl2_party-leader.svg?react";
 import IconOpenEye from "../../assets/icon/el_eye-open.svg?react";
@@ -7,6 +7,8 @@ import IconEdit from "../../assets/icon/flowbite_edit-outline.svg?react";
 import IconDelete from "../../assets/icon/material-symbols_delete.svg?react";
 
 function TdUser({ no, name, email, jabatan, status, password }) {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <tr>
       <td>{no}.</td>
@@ -27,8 +29,21 @@ function TdUser({ no, name, email, jabatan, status, password }) {
 
       <td>
         <div className="password-column">
-          <p>{password}</p>
-          <IconOpenEye className="icon blackIcon" />
+          <p>{showPassword ? password : "******"}</p>
+
+          {showPassword ? (
+            <IconCloseEye
+              className="icon blackIcon"
+              onClick={() => setShowPassword(false)}
+              style={{ cursor: "pointer" }}
+            />
+          ) : (
+            <IconOpenEye
+              className="icon blackIcon"
+              onClick={() => setShowPassword(true)}
+              style={{ cursor: "pointer" }}
+            />
+          )}
         </div>
       </td>
 
