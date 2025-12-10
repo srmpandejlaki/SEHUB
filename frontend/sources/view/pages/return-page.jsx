634d62957d77b757955d3c744412ae8b7c5127d5
Muti <mutiarapandejlaki47@gmail.com>
2025-12-10 20:50:48 +0800

    perbaiki tampilan nav

--- frontend/sources/view/pages/return-page.jsx
+++ frontend/sources/view/pages/return-page.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SearchFilter from "../../components/base/search-filter";
-import ProductItems from "../../components/product-page/product-items";
 import NavProduct from "../../components/base/nav-product";
 
 function ReturnPage() {
@@ -13,7 +12,7 @@ function ReturnPage() {
         </div>
         <SearchFilter />
       </div>
-      
+
     </div>
   );
 }
