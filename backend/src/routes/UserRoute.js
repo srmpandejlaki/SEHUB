2c3e3b77df02fcbe8caf56298ad33871631bd703
Muti <mutiarapandejlaki47@gmail.com>
2025-12-14 21:00:02 +0800

    ternyata routernya lupa diubah

--- backend/src/routes/UserRoute.js
+++ backend/src/routes/UserRoute.js
@@ -4,10 +4,10 @@ import UserController from "../controllers/UserController.js";
 const routerUsers = express.Router();
 
 routerUsers.get("/", UserController.getAllUser);
-routerUsers.get("/:id_user", UserController.getUserById);
+routerUsers.get("/:id_pengguna", UserController.getUserById);
 routerUsers.post("/", UserController.createUser);
-routerUsers.put("/:id_user", UserController.updateUser);
-routerUsers.delete("/:id_user", UserController.deleteUser);
+routerUsers.put("/:id_pengguna", UserController.updateUser);
+routerUsers.delete("/:id_pengguna", UserController.deleteUser);
 routerUsers.delete("/", UserController.deleteAllUser);
 
 export default routerUsers;
