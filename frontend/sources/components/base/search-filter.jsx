503ba7c98f88af0b5d8baac8960c8acb87db603d
Muti <mutiarapandejlaki47@gmail.com>
2025-12-15 00:04:09 +0800

    hapus tombol filter

--- frontend/sources/components/base/search-filter.jsx
+++ frontend/sources/components/base/search-filter.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import IconSearch from "../../assets/icon/ic_round-search.svg?react";
-import IconFilter from "../../assets/icon/line-md_filter.svg?react";
 
 function SearchFilter() {
   return (
@@ -8,10 +7,6 @@ function SearchFilter() {
       <div className="search">
         <IconSearch className="icon blackIcon" />
         <input type="text" placeholder="Search" />
-      </div>
-      <div className="filter">
-        <IconFilter className="icon whiteIcon" />
-        <input type="text" placeholder="Filter" />
       </div>
     </div>
   );
