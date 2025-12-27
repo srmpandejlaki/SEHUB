27dd4cceb41c8d8464d87338c0f68acf38cb0d43
Muti <mutiarapandejlaki47@gmail.com>
2025-12-27 15:45:19 +0800

    styling sedikit bagian pengaturan master data

--- frontend/sources/components/setting-section/master-data.jsx
+++ frontend/sources/components/setting-section/master-data.jsx
@@ -9,9 +9,37 @@ function MasterData() {
       <div className="main-data-setting">
         <div className="nama-produk">
           <p>Daftar Nama Produk <br/>Silahkan klik "tambah produk" untuk nama produk baru.</p>
+          <div className="list">
+            <ul>
+              <li>seho sirop</li>
+              <li>seho granule</li>
+              <li>seho block</li>
+            </ul>
+          </div>
+          <button>tambah produk</button>
         </div>
-        <div className="ukuran-satuan"></div>
-        <div className="jenis-kemasan"></div>
+        <div className="ukuran-satuan">
+          <p>Daftar Ukuran Satuan <br/>Silahkan klik "tambah ukuran" untuk membuat ukuran baru.</p>
+          <div className="list">
+            <ul>
+              <li>ml</li>
+              <li>kg</li>
+              <li>gr</li>
+            </ul>
+          </div>
+          <button>tambah produk</button>
+        </div>
+        <div className="jenis-kemasan">
+          <p>Daftar Jenis Kemasan <br/>Silahkan klik "tambah kemasan" untuk jenis kemasan baru.</p>
+          <div className="list">
+            <ul>
+              <li>botol</li>
+              <li>pouch</li>
+              <li>toples</li>
+            </ul>
+          </div>
+          <button>tambah produk</button>
+        </div>
       </div>
     </div>
   );
